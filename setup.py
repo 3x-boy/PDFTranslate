e9f38882377f311b0de653c9381e1426b527166b
3x-boy <55905419+3x-boy@users.noreply.github.com>
2020-01-28 20:18:25 +0900

    Changes to publish as a library

--- setup.py
+++ setup.py
@@ -7,15 +7,16 @@ with open(path.join(here, 'README.md'),mode='r', encoding='utf-8') as fr:
     long_description = fr.read()
 
 setuptools.setup(
-    namw="PDFTranslate",
+    name="PDFTranslate",
     version="0.1.0",
     description="PDFTranslate Location List",
     long_description=long_description,
+    packages=setuptools.find_packages(),
     url="https://github.com/3x-boy/PDFTranslate",
     author="3x_boy",
-    author_email="xxxdayiwasbornin@gmail.com",
     keywords="PDFTranslate",
     license="MIT",
+    install_requires = ['googletrans', 'pyPDF2'],
     classifiers=[
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python :: 3.7.3',
